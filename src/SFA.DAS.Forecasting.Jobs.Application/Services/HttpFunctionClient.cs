284622203feb1702d18ba74b8531f0c00dc27015
Jon Harris <jon.harris@digital.education.gov.uk>
2023-12-05 10:41:04 +0000

    Refactoring to add headers to the httpRequest instead of the httpClient.

--- src/SFA.DAS.Forecasting.Jobs.Application/Services/HttpFunctionClient.cs
+++ src/SFA.DAS.Forecasting.Jobs.Application/Services/HttpFunctionClient.cs
@@ -1,8 +1,8 @@
-﻿using Newtonsoft.Json;
-using SFA.DAS.Forecasting.Domain.Infrastructure;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using SFA.DAS.Forecasting.Domain.Infrastructure;
 
 namespace SFA.DAS.Forecasting.Jobs.Application.Services;
 
@@ -13,19 +13,22 @@ public class HttpFunctionClient<T> : IHttpFunctionClient<T>
     public async Task<HttpResponseMessage> PostAsync(string url, T data)
     {
         const string mediaType = "application/json";
-        var content = new StringContent(JsonConvert.SerializeObject(data));
 
-        var client = new HttpClient();
-        client.DefaultRequestHeaders
+        using var content = new StringContent(JsonConvert.SerializeObject(data));
+        using var client = new HttpClient();
+
+        using var httpRequest = new HttpRequestMessage(HttpMethod.Post, url);
+        httpRequest.Content = content;
+
+        httpRequest.Headers
             .Accept
             .Add(new MediaTypeWithQualityHeaderValue(mediaType));
-
+        
         if (!string.IsNullOrEmpty(XFunctionsKey))
         {
-            client.DefaultRequestHeaders.Add("x-functions-key", XFunctionsKey);
+            httpRequest.Headers.Add("x-functions-key", XFunctionsKey);
         }
 
-        return await client.PostAsync(url, content);
+        return await client.SendAsync(httpRequest);
     }
-
 }
