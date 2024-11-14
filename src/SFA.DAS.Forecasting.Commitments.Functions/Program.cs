587dbcfdf799772739047e44e5623e77e45daa1a
Jon Harris <jon.harris@fuitsu.com>
2024-11-14 15:18:39 +0000

    Refactoring startup for functions project

---
+++ src/SFA.DAS.Forecasting.Commitments.Functions/Program.cs
@@ -0,0 +1,56 @@
+using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+using SFA.DAS.CommitmentsV2.Api.Client;
+using SFA.DAS.Forecasting.Commitments.Functions.StartupExtensions;
+using SFA.DAS.Forecasting.Domain.CommitmentsFunctions;
+using SFA.DAS.Forecasting.Domain.CommitmentsFunctions.Services;
+using SFA.DAS.Forecasting.Jobs.Application.CommitmentsFunctions.Handlers;
+using SFA.DAS.Forecasting.Jobs.Application.CommitmentsFunctions.Handlers.Services;
+using SFA.DAS.Forecasting.Jobs.Application.CommitmentsFunctions.Mapper;
+
+[assembly: NServiceBusTriggerFunction("SFA.DAS.Forecasting.Commitments.Functions")]
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureAppConfiguration(builder => builder.BuildDasConfiguration())
+    .ConfigureNServiceBus()
+    .ConfigureServices((context, services) =>
+    {
+        services.AddDasLogging();
+
+        var configuration = context.Configuration;
+
+        var environment = configuration["EnvironmentName"];
+
+        services.AddSingleton<IConfiguration>(configuration);
+
+        var serviceProvider = services.BuildServiceProvider();
+
+        var mapperConfig = new MapperConfiguration(x => x.AddProfile<AutoMapperProfile>());
+
+        var mapper = mapperConfig.CreateMapper();
+        services.AddSingleton(mapper);
+
+        services.AddScoped(_ => configuration.CreateDocumentSession());
+
+        var commitmentsClientApiConfig = configuration.GetCommitmentsClientApiConfiguration(services);
+        var loggingFactory = serviceProvider.GetService<ILoggerFactory>();
+        services.AddSingleton<ICommitmentsApiClientFactory>(x => new CommitmentsApiClientFactory(commitmentsClientApiConfig, loggingFactory));
+        services.AddTransient<ICommitmentsApiClient>(provider => provider.GetRequiredService<ICommitmentsApiClientFactory>().CreateClient());
+
+        services.AddScoped<IApprenticeshipCompletedEventHandler, ApprenticeshipCompletedEventHandler>();
+        services.AddScoped<IApprenticeshipStoppedEventHandler, ApprenticeshipStoppedEventHandler>();
+        services.AddScoped<IApprenticeshipStopDateChangedEventHandler, ApprenticeshipStopDateChangedEventHandler>();
+        services.AddScoped<IApprenticeshipCompletionDateUpdatedEventHandler, ApprenticeshipCompletionDateUpdatedEventHandler>();
+
+        services.AddScoped<IGetApprenticeshipService, GetApprenticeshipService>();
+
+        services.AddDatabaseRegistration(configuration, environment);
+    })
+    .Build();
+
+host.Run();
---
+++ src/SFA.DAS.Forecasting.Commitments.Functions/StartupExtensions/DocumentSessionExtensions.cs
@@ -0,0 +1,35 @@
+using System;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Extensions.Configuration;
+using SFA.DAS.Forecasting.Jobs.Infrastructure.CosmosDB;
+
+namespace SFA.DAS.Forecasting.Commitments.Functions.StartupExtensions;
+
+public static class DocumentSessionExtensions
+{
+    public static IDocumentSession CreateDocumentSession(this IConfiguration config)
+    {
+        var connectionString = config["CosmosDbConnectionString"];
+
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new InvalidOperationException("No 'DocumentConnectionString' connection string found.");
+        }
+
+        var documentConnectionString = new DocumentSessionConnectionString(connectionString);
+
+        var client = new DocumentClient(new Uri(documentConnectionString.AccountEndpoint),
+            documentConnectionString.AccountKey);
+        client.CreateDatabaseIfNotExistsAsync(new Database { Id = documentConnectionString.Database }).Wait();
+
+        client.CreateDocumentCollectionIfNotExistsAsync(
+            UriFactory.CreateDatabaseUri(documentConnectionString.Database), new DocumentCollection
+            {
+                Id = documentConnectionString.Collection
+            },
+            new RequestOptions { OfferThroughput = int.Parse(documentConnectionString.ThroughputOffer) }).Wait();
+
+        return new DocumentSession(client, documentConnectionString);
+    }
+}
