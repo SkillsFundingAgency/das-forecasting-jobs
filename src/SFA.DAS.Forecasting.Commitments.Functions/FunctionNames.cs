b0cc13cdb4a74f08eb826b56991eb3baeacd26c0
Jon Harris <jon.harris@fuitsu.com>
2024-11-27 08:19:21 +0000

    Removing unused code

--- src/SFA.DAS.Forecasting.Commitments.Functions/FunctionNames.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SFA.DAS.Forecasting.Commitments.Functions;
-
-public static class FunctionNames
-{
-    public const string ApprenticeshipCompleted = nameof(ApprenticeshipCompleted);
-    public const string ApprenticeshipCompletionDateUpdated = nameof(ApprenticeshipCompletionDateUpdated);
-    public const string ApprenticeshipStopDateChanged = nameof(ApprenticeshipStopDateChanged);
-    public const string ApprenticeshipStopped = nameof(ApprenticeshipStopped);
-}
