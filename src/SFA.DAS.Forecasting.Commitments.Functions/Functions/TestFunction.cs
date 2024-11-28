27b1a8e6435e800c823edbae46c7db804e84f3f0
Jon Harris <jon.harris@fuitsu.com>
2024-11-28 11:02:12 +0000

    Fixing test functions

--- src/SFA.DAS.Forecasting.Commitments.Functions/Functions/TestFunction.cs
+++ src/SFA.DAS.Forecasting.Commitments.Functions/Functions/TestFunction.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using SFA.DAS.CommitmentsV2.Messages.Events;
@@ -11,13 +12,13 @@ namespace SFA.DAS.Forecasting.Commitments.Functions.Functions;
 public class TestFunction(ILogger<TestFunction> logger, IApprenticeshipCompletedEventHandler handler)
 {
     [Function("TestCosmosDbFunction")]
-    public async Task Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = "TestCosmosDbFunction")] HttpRequestMessage req)
+    public async Task Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = "TestCosmosDbFunction")] HttpRequestMessage req, [FromQuery] long apprenticeshipId)
     {
         logger.LogInformation("TestCosmosDbFunction function executed.");
 
         await handler.Handle(new ApprenticeshipCompletedEvent
         {
-            ApprenticeshipId = 100,
+            ApprenticeshipId = apprenticeshipId,
             CompletionDate = DateTime.UtcNow
         });
     }
